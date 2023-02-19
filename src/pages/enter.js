410b03aeb5e3e688d3150f845443cbd712980ead
dloyolar <dieegolr@gmail.com>
2023-02-19 13:56:06 -0300

    ✨ feat(enter): add button to signIn

--- src/pages/enter.js
+++ src/pages/enter.js
@@ -1,9 +1,45 @@
+import { auth, googleAuthProvider } from '@/lib/firebase';
+import { signInWithPopup, signOut } from 'firebase/auth';
+import Image from 'next/image';
+
 const Enter = () => {
+  const user = null;
+  const username = null;
+
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 };
 
 export default Enter;
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} alt="Google Logo" />
+      Sign in with Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
+}
